75a3fa65942449f065002ae68bd94c452c8cbd67
FridericHansel <friderichansel@gmail.com>
2022-05-16 07:33:06 +0700

    3.2.Mengganti arsitektur menjadi MVVM

--- app/src/main/java/org/d3if1012/galerihewan/ui/main/MainAdapter.kt
+++ app/src/main/java/org/d3if1012/galerihewan/ui/main/MainAdapter.kt
@@ -8,8 +8,13 @@ import org.d3if1012.galerihewan.R
 import org.d3if1012.galerihewan.databinding.ListItemBinding
 import org.d3if1012.galerihewan.model.Hewan
 
-class MainAdapter (private val data: List<Hewan>):
-    RecyclerView.Adapter<MainAdapter.ViewHolder>() {
+class MainAdapter : RecyclerView.Adapter<MainAdapter.ViewHolder>() {
+    private val data = mutableListOf<Hewan>()
+    fun updateData(newData: List<Hewan>) {
+        data.clear()
+        data.addAll(newData)
+        notifyDataSetChanged()
+    }
     class ViewHolder(
         private val binding: ListItemBinding
     ) : RecyclerView.ViewHolder(binding.root) {
