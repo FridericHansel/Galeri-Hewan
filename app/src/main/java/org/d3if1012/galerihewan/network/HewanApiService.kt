f1ac16972bd3a94b4707745f86108be245d36978
FridericHansel <friderichansel@gmail.com>
2022-05-27 09:22:47 +0700

    3.4.Menambahkan loading indicator

--- app/src/main/java/org/d3if1012/galerihewan/network/HewanApiService.kt
+++ app/src/main/java/org/d3if1012/galerihewan/network/HewanApiService.kt
@@ -3,6 +3,8 @@ package org.d3if1012.galerihewan.network
 import com.squareup.moshi.Moshi
 import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
 import org.d3if1012.galerihewan.model.Hewan
+import retrofit2.Retrofit
+import retrofit2.http.GET
 
 private const val BASE_URL = "https://raw.githubusercontent.com/" +
         "indraazimi/galeri-hewan/static-api/"
