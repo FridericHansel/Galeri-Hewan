197bd4b29398217c8bf12d66affe9e8d97cc3994
FridericHansel <friderichansel@gmail.com>
2022-05-27 08:53:07 +0700

    3.2.Memproses data internet sebagai JSON

--- app/src/main/java/org/d3if1012/galerihewan/model/Hewan.kt
+++ app/src/main/java/org/d3if1012/galerihewan/model/Hewan.kt
@@ -1,8 +1,10 @@
 package org.d3if1012.galerihewan.model
 
+import org.d3if1012.galerihewan.R
+
 data class Hewan(
     val nama: String,
     val namaLatin: String ,
     val jenis: String,
-    val imageResId: Int
+    val imageResId: Int = R.drawable.angsa
 )
--- app/src/main/java/org/d3if1012/galerihewan/network/HewanApiService.kt
+++ app/src/main/java/org/d3if1012/galerihewan/network/HewanApiService.kt
@@ -1,16 +1,24 @@
 package org.d3if1012.galerihewan.network
 
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import org.d3if1012.galerihewan.model.Hewan
+
 private const val BASE_URL = "https://raw.githubusercontent.com/" +
         "indraazimi/galeri-hewan/static-api/"
 
+private val moshi = Moshi.Builder()
+    .add(KotlinJsonAdapterFactory())
+    .build()
+
 private val retrofit = Retrofit.Builder()
-    .addConverterFactory(ScalarsConverterFactory.create())
+    .addConverterFactory(MoshiConverterFactory.create(moshi))
     .baseUrl(BASE_URL)
     .build()
 
 interface HewanApiService {
     @GET("static-api.json")
-    suspend fun getHewan(): String
+    suspend fun getHewan(): List<Hewan>
 }
 
 object HewanApi {
