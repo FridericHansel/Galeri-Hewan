dff9d71cb9296e32d34cbe222768a8c567c0816e
FridericHansel <friderichansel@gmail.com>
2022-06-03 11:20:02 +0700

    3.1.Mempelajari daur hidup Activity

--- app/src/main/java/org/d3if1012/galerihewan/ui/main/MainActivity.kt
+++ app/src/main/java/org/d3if1012/galerihewan/ui/main/MainActivity.kt
@@ -2,15 +2,39 @@ package org.d3if1012.galerihewan.ui.main
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.RecyclerView
 import org.d3if1012.galerihewan.R
-import org.d3if1012.galerihewan.databinding.ActivityMainBinding
 import org.d3if1012.galerihewan.model.Hewan
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Log.i("MainActivity", "onCreate dijalankan")
+
     }
+    override fun onStart() {
+        super.onStart()
+        Log.i("MainActivity", "onStart dijalankan")
+    }
+    override fun onResume() {
+        super.onResume()
+        Log.i("MainActivity", "onResume dijalankan")
+    }
+    override fun onPause() {
+        Log.i("MainActivity", "onPause dijalankan")
+        super.onPause()
+    }
+    override fun onStop() {
+        Log.i("MainActivity", "onStop dijalankan")
+        super.onStop()
+    }
+    override fun onDestroy() {
+        Log.i("MainActivity", "onDestroy dijalankan")
+        super.onDestroy()
+    }
+
 }
